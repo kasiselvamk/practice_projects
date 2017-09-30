47386f8d0a3491a29252c976c121351b350eb34d
kasi <kasi@localhost>
2017-09-30 19:41:08 +0530

    commit

---
+++ TamilDic/src/com/monkmonkeys/main/GetData.java
@@ -0,0 +1,36 @@
+package com.monkmonkeys.main;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+public class GetData {
+
+	public static void main(String[] args) throws ClientProtocolException, IOException {
+ 
+		//http://www.tamilvu.org/library/l0B00/html/l0B00ind.htm
+		String url = "http://www.tamilvu.org/slet/servlet/lexpg?pageno=";
+
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpGet request = null;HttpResponse response=null;HttpEntity e=null; InputStream data =null;
+		//4351
+		for(Integer i=1;i<=4351;i++) {
+			 request = new HttpGet(url+i);
+			 response = client.execute(request);
+	           FileUtils.copyInputStreamToFile(response.getEntity().getContent(), new File("/home/kasi/eclipse-workspace/TamilDic/data/"+i+".html"));
+			   System.out.println(i +" Done");
+		}
+		
+	 
+           
+	}
+
+}
